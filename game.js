2fd258445c1336c02446b63affd6c0ac0b96aee7
Immanuel156758 <161232399+Immanuel156758@users.noreply.github.com>
2024-05-05 16:14:02 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent("YouWin", {
+AFRAME.registerComponent("youwin", {
 
 	init: function (){
 		turtleCount = 5;
@@ -6,7 +6,7 @@ AFRAME.registerComponent("YouWin", {
 			e.detail.target.el.remove();
 			turtleCount = turtleCount - 1;
 			turtleNumber = document.getElementById("number")
-			turtleNumber.setAttribute('text',{value: String(tutleCount)});
+			turtleNumber.setAttribute('text',{value: String(turtleCount)});
 			if(turtleCount == 0){
 				turtleEnd = document.getElementById("end")
 				turtleEnd.setAttribute('text',{value: 'You Win!!'})
